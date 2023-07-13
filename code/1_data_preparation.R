88f5ea25fa49f4d166acec67f8448433e2519d5b
Lea Bergmann <lea.bergmann@rwi-essen.de>
2023-07-13 14:46:23 +0200

    tried different imputation methods Imputated_data1 appears to deliver the best result (method = pmm, dataset without dummies) Further imputation checks necessary

--- code/1_data_preparation.R
+++ code/1_data_preparation.R
@@ -6,11 +6,12 @@ setwd("C:/Users/lbergmann/OneDrive - RWI–Leibniz-Institut für Wirtschaftsfors
 library(dplyr)
 library(GGally)
 library(mice)
+library(beepr)
 
 # import ds
 ds <- read.csv('data/gss_wages_train.csv')
 summary(ds)
-
+ds1 <- ds
 cat("the dataset has ", nrow(ds), 'rows, and ', round(sum(is.na(ds$realrinc))/nrow(ds)*100,2), "% of missing values for the outcome \n")
 
 
@@ -18,7 +19,7 @@ cat("the dataset has ", nrow(ds), 'rows, and ', round(sum(is.na(ds$realrinc))/nr
 ###test 1
 #occupation
 #ds_occ1 <- ds %>% distinct(occ10) #there are 537 different occupations
-ds_occ2 <- ds %>% distinct(occrecode)#there are 12 different occupations: Office, Professional, NA, Business/Finance, Construction, Sales, Transportation, Service, Production, Farming, Installation, Armed Forces
+#ds_occ2 <- ds %>% distinct(occrecode)#there are 12 different occupations: Office, Professional, NA, Business/Finance, Construction, Sales, Transportation, Service, Production, Farming, Installation, Armed Forces
 ds$Office <-  ifelse(ds$occrecode == "Office and Administrative Support", 1, 0)
 ds$Office <- ifelse(is.na(ds$occrecode) | ds$Office == 0, 0, 1)
 ds$Professional <-  ifelse(ds$occrecode == "Professional", 1, 0)
@@ -86,15 +87,6 @@ ds$unmarried <- ifelse(is.na(ds$maritalcat) | ds$unmarried == 0,0,1)
 #gender
 ds$female <- ifelse(ds$gender == "Female", 1, 0)
 
-#age
-ds$age2 <- (ds$age)^2
-summary(ds$age2)
-summary(ds$age)
-
-#log income
-ds$linc <- log(ds$realrinc)
-summary(ds$linc)
-summary(ds$realrinc)
 
 #NA analysis: 33066 obs for which we have all variables, 17715 only miss income, 2977 miss occupation, income &prestige
 #             382 obs miss prestige, 159 miss income & prestige
@@ -108,8 +100,11 @@ summary(ds_na$realrinc)
 ds_na <- ds_na[!is.na(ds$realrinc),]
 
 # filtered dataset, not using obs, with missing value for outcome
-ds_filter <- ds[!is.na(ds$realrinc),]
+ds_filter  <- ds[!is.na(ds$realrinc),] #filtered dataset
+ds_filter1 <- ds1[!is.na(ds1$realrinc),] #filtered dataset based on dataset without dummies
 summary(ds_filter)
+summary(ds_filter1)
+ds_filter1$female <- ifelse(ds_filter1$gender == "Female", 1, 0)
 
 cat("the dataset has ", nrow(ds_filter), 'rows \n')
 summary(ds_filter)
@@ -117,14 +112,82 @@ summary(ds_filter)
 #NA anyalysis for filtered dataset: 33066 obs for which we have all variables, 382 obs miss prestige, 82 miss occupation and prestige, 71 miss children
 md.pattern(ds_filter)
 
+#NA analysis: children
+ds_child <- ds_filter %>% filter(is.na(childs))
+summary(ds_child)
+###those for whom we don't know whether they have children are mainly in their 30s, male and work full-time
+
+#NA analysis: prestige
+ds_prest <- ds_filter %>% filter(is.na(prestg10))
+###most people for whom we don't know the prestige have code 9997 which implies that their occupation cannot be coded --> I would suggest to remove them
+ds_filter2 <- ds_filter %>% filter(occ10 != 9997)
+summary(ds_filter2)
+
 #imputation using mice
 imputated_data <- mice(ds_filter, m = 5, method = "pmm", maxit = 50) #predictive mean matching
-imputated_data$imp #inspect imputated values
-Data <- complete(imputated_data,1) #create dataset including imputated values
+imputated_data1 <- mice(ds_filter1, m = 5, method = "pmm", maxit = 50) #predicte mean matching without dummies --> best results
+imputated_data2 <- mice(ds_filter2, m = 5, method = "pmm", maxit = 50) #predictive mean matching, excluding occ10 = 9997
+imputated_data3 <- mice(ds_filter, m = 20, method = "pmm", maxit = 50) # more impuations only improve the result for age
+imputated_data4 <- mice(ds_filter, m = 5, method = "norm", maxit = 20) #method norm does not improve the results
+imputated_data5 <- mice(ds_filter, m = 5, method = "cart", maxit = 20) #method norm does not improve the results
+imputated_data6 <- mice(ds_filter, m = 5, method = "rf", maxit = 20) #method norm does not improve the results
+imputated_data7 <- mice(ds_filter, m = 5, method = "lasso.norm", maxit = 20) #method norm does not improve the results
+#imputated_data8 <- mice(ds_filter, m = 5, method = "logreg", maxit = 20) #does not work (maximum number of categories exceeded)
+#imputated_data9 <- mice(ds_filter, m = 5, method = "lasso.logreg", maxit = 20) #does not work (more than two classes)
+#imputated_data10 <- mice(ds_filter, m = 5, method = "polr", maxit = 20) #does not work (maximum number of categories exceeded)
+#imputated_data11 <- mice(ds_filter, m = 5, method = "polyreg", maxit = 20) #does not work (maximum number of categories exceeded)
+#imputated_data12 <- mice(ds_filter, m = 5, method = "lda", maxit = 20) #does not work: only categorical data
+imputated_data13 <- mice(ds_filter, m = 5, method = c("", "", "pmm", "pmm", "", "pmm", "norm", "", "", "", "")) #results in negativ results for childs
+
+ini <- mice(ds, maxit = 50, pri = FALSE)
+pred <- ini$pred
+pred[, "realrinc"] <- 0
+meth <- ini$meth
+meth["realrinc"] <- ""
+imputated_data3 <- mice(ds, m = 5, meth = meth, pred = pred, pri = FALSE)
+
+###should we remove any predictors?
+###age2 should be created after imputation
+
+
+#inspect imputated values
+Data <- complete(imputated_data,1) #create dataset including imputated values from first imputation
+Data2 <- complete(imputated_data13,1)
+Data3 <- complete(imputated_data3, 1)
 
 #inspect imputation
-xyplot(imputated_data,realrinc ~ age + occ10 + prestg10 +childs,pch=18,cex=1)
+xyplot(imputated_data,realrinc ~ age| .imp, pch = c(1, 20), cex = c(0.8, 1.2)) #does not work: I can't see any magenta points
+
 densityplot(imputated_data)
+densityplot(imputated_data2)
+densityplot(imputated_data3)
+densityplot(imputated_data4)
+densityplot(imputated_data5) #sehr schlecht: negative Werte für childs
+densityplot(imputated_data6) #fit gefällt mir für die meisten imputations
+densityplot(imputated_data7) #sehr schlecht: negative Werte für childs
+densityplot(imputated_data13)
+
+#stripplots don't work
+stripplot(imputated_data2)
+stripplot(imputated_data3)
+stripplot(imputated_data4)
+stripplot(imputated_data5)
+stripplot(imputated_data6)
+stripplot(imputated_data7)
+
+
+
+
+#age
+ds$age2 <- (ds$age)^2
+summary(ds$age2)
+summary(ds$age)
+
+
+#log income
+ds$linc <- log(ds$realrinc)
+summary(ds$linc)
+summary(ds$realrinc)
 
 # fast estimation of correlation matrix
 cor.filter <- select_if(ds_filter, is.numeric)
