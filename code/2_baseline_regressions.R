c634c7463605196480397220d198bd2a3438a0b3
Lea Bergmann <lea.bergmann@rwi-essen.de>
2023-07-27 16:27:25 +0200

    more regressions with final dataset

--- code/2_baseline_regressions.R
+++ code/2_baseline_regressions.R
@@ -21,17 +21,29 @@ valid_test_index_filter <- createDataPartition(y = remaining_filter$realrinc, p 
 validation_filter <- remaining_filter[valid_test_index_filter, ]
 test_filter <- remaining_filter[-valid_test_index_filter, ]
 
-trainindex_na <- createDataPartition(y = ds_na$realrinc, p = 0.7, list = FALSE) # Split the data into training (70%) and remaining (30%)
-train_na <- ds_na[trainindex_na, ]
-remaining_na <- ds_na[-trainindex_na, ]
-
-valid_test_index_na <- createDataPartition(y = remaining_na$realrinc, p = 0.5, list = FALSE) # Split the remaining data into validation (50%) and test (50%)
-validation_na <- remaining_na[valid_test_index_na, ]
-test_na <- remaining_na[-valid_test_index_na, ]
 
 #OLS regressions
-m1 <- lm(realrinc ~ . - occrecode -wrkstat - gender -educcat -maritalcat, data = train_na )
-summary(m1)
+mod_full <- lm(realrinc ~ . - occrecode -wrkstat - gender -educcat -maritalcat, data = ds)
+summary(mod_full)
 
-m2 <- with(Data,lm(realrinc ~ . - occrecode -wrkstat - gender -educcat -maritalcat))
+mod_wo_interactions <- lm(realrinc ~ age + prestg10 + childs + armed_forces + business_finance + construction_extraction + farming_fishing_and_forestry + 
+                            installation_maintenance_and_repair + office_and_administrative_support + production + professional + sales + service + 
+                             + full_time + housekeeper + part_time + retired + school + temporarily_not_working + unemployed_laid_off + bachelor + 
+                            graduate + highschool + juniorcollege +  + divorced + married + nevermarried + separated  + female, data = ds)
+summary(mod_wo_interactions)
+
+mod_loginc <- lm(log_realrinc ~ age + prestg10 + childs + armed_forces + business_finance + construction_extraction + farming_fishing_and_forestry + 
+                   installation_maintenance_and_repair + office_and_administrative_support + production + professional + sales + service + 
+                   + full_time + housekeeper + part_time + retired + school + temporarily_not_working + unemployed_laid_off + bachelor + 
+                   graduate + highschool + juniorcollege + divorced + married + nevermarried + separated  + female, data = ds)
+summary(mod_loginc)
+
+mod_age2 <- lm(realrinc ~ age + age_sqr + prestg10 + childs + armed_forces + business_finance + construction_extraction + farming_fishing_and_forestry + 
+                 installation_maintenance_and_repair + office_and_administrative_support + production + professional + sales + service + 
+                 + full_time + housekeeper + part_time + retired + school + temporarily_not_working + unemployed_laid_off + bachelor + 
+                 graduate + highschool + juniorcollege +  divorced + married + nevermarried + separated  + female, data = ds)
+summary(mod_age2)
+
+
+m2 <- with(ds,lm(realrinc ~ . - occrecode -wrkstat - gender -educcat -maritalcat)) ### this does not work for me
 summary(pool(m2))
